40423c99ffad82eb59941709e961908cf44d9f51
unknown <juanjosec@unicauca.edu.co>
2022-11-28 12:00:31 -0500

    Reto 2

---
+++ Minireto1JAVA/Challange1/Challange1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    RamdonCharacter test = new RamdonCharacter();
+        System.out.println(test.randomText("Tipo A"));
+        System.out.println(test.randomText("Tipo B"));
+
+
+    }
+}
